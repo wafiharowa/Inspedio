d9ed033760dce16b89f4e1da9c2b298f69873a26
Wafi Harowa <harowa_aja@yahoo.co.id>
2014-01-13 09:12:31 +0700

    Add Up-Down support for InsSwipe

--- src/com/inspedio/entity/ui/InsSwipe.java
+++ src/com/inspedio/entity/ui/InsSwipe.java
@@ -3,6 +3,7 @@ package com.inspedio.entity.ui;
 import com.inspedio.entity.InsShape;
 import com.inspedio.entity.primitive.InsCallback;
 import com.inspedio.entity.primitive.InsPoint;
+import com.inspedio.system.helper.InsUtil;
 
 public class InsSwipe extends InsShape{
 
@@ -18,11 +19,27 @@ public class InsSwipe extends InsShape{
 	 */
 	protected InsCallback onSwipeRight;
 	/**
+	 * Callback event when UI Swiped to Up
+	 */
+	protected InsCallback onSwipeUp;
+	/**
+	 * Callback event when UI Swiped to Down
+	 */
+	protected InsCallback onSwipeDown;
+	
+	/**
 	 * Set this to FALSE to let object behind it to get Event. Defaulted to false
 	 */
 	public boolean onSwipedReturn = false;
 	
-	public int  tolerance = 40;
+	/**
+	 * How much X distance user have to swipe to consider it swiped
+	 */
+	public int  toleranceX = 40;
+	/**
+	 * How much Y distance user have to swipe to consider it swiped
+	 */
+	public int  toleranceY = 40;
 	
 	public InsSwipe(){
 		this(0, 0, 0, 0);
@@ -49,6 +66,20 @@ public class InsSwipe extends InsShape{
 	}
 	
 	/**
+	 * Set Callback to handle Swipe Up Event
+	 */
+	public void setSwipedUpCallback(InsCallback c){
+		this.onSwipeUp = c;
+	}
+	
+	/**
+	 * Set Callback to handle Swipe Down Event
+	 */
+	public void setSwipedDownCallback(InsCallback c){
+		this.onSwipeDown = c;
+	}
+	
+	/**
 	 * Do not override this unless you want to specifically access coordinate touched
 	 */
 	public boolean onPointerPressed(int X, int Y) {	
@@ -68,14 +99,36 @@ public class InsSwipe extends InsShape{
 		return false;
 	}
 	
+	/**
+	 * Swipe Event checked Prioritize X over Y
+	 */
 	public boolean evaluate(){
 		int dX = endPoint.x - startPoint.x;
-		if(dX >= tolerance){
-			this.onSwipeRight.call();
-			return onSwipedReturn;
-		} else if(dX <= -tolerance){
-			this.onSwipeLeft.call();
-			return onSwipedReturn;
+		
+		if(dX >= toleranceX){
+			if(this.onSwipeRight != null){
+				this.onSwipeRight.call();
+				return onSwipedReturn;
+			}
+		} else if(dX <= -toleranceX){
+			if(this.onSwipeLeft != null){
+				this.onSwipeLeft.call();
+				return onSwipedReturn;
+			}
+		}
+		
+		int dY = endPoint.y - startPoint.y;
+		
+		if(dY >= toleranceY){
+			if(this.onSwipeDown != null){
+				this.onSwipeDown.call();
+				return onSwipedReturn;
+			}
+		} else if(dY <= -toleranceY){
+			if(this.onSwipeUp != null){
+				this.onSwipeUp.call();
+				return onSwipedReturn;
+			}
 		}
 		
 		return false;
@@ -84,7 +137,8 @@ public class InsSwipe extends InsShape{
 	/**
 	 * Set Tolerance Value (minimum distance swiped to consider valid)
 	 */
-	public void setTolerance(int Val){
-		this.tolerance = Val;
+	public void setTolerance(int ToleranceX, int ToleranceY){
+		this.toleranceX = InsUtil.Absolute(ToleranceX);
+		this.toleranceY = InsUtil.Absolute(ToleranceY);
 	}
 }
